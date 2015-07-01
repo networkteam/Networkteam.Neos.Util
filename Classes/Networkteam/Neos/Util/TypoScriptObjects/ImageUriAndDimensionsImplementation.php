5948ddbd3214e139caf050c1c2fafa1f0ee7c889
Christopher Hlubek <hlubek@networkteam.com>
2015-07-01 15:01:39 +0200

    [FEATURE] Add custom TypoScript object for full image information
    The TYPO3.Neos:ImageUri object does only return the URI and no dimension
    information.
    
    Example::
    
    	@override.popoverImage = Networkteam.Neos.Util:ImageUriAndDimensions {
    		maximumWidth = 1280
    		maximumHeight = 800
    		asset = ${q(node).property('image').originalAsset || q(node).property('image')}
    	}
    
    	attributes {
    		data-popover-src = ${popoverImage.src}
    		data-popover-width = ${popoverImage.width}
    		data-popover-height = ${popoverImage.height}
    	}
    
    Change-Id: I510f9612d4357b55b3291bffc245cd5ca0a7c3f4

---
+++ Classes/Networkteam/Neos/Util/TypoScriptObjects/ImageUriAndDimensionsImplementation.php
@@ -0,0 +1,85 @@
+<?php
+namespace Networkteam\Neos\Util\TypoScriptObjects;
+
+/***************************************************************
+ *  (c) 2014 networkteam GmbH - all rights reserved
+ ***************************************************************/
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Media\Domain\Model\AssetInterface;
+use TYPO3\Media\Domain\Service\AssetService;
+
+class ImageUriAndDimensionsImplementation extends \TYPO3\TypoScript\TypoScriptObjects\AbstractTypoScriptObject {
+
+	/**
+	 * Resource publisher
+	 *
+	 * @Flow\Inject
+	 * @var AssetService
+	 */
+	protected $assetService;
+
+	/**
+	 * Asset
+	 *
+	 * @return AssetInterface
+	 */
+	public function getAsset() {
+		return $this->tsValue('asset');
+	}
+
+	/**
+	 * MaximumWidth
+	 *
+	 * @return integer
+	 */
+	public function getMaximumWidth() {
+		return $this->tsValue('maximumWidth');
+	}
+
+	/**
+	 * MaximumHeight
+	 *
+	 * @return integer
+	 */
+	public function getMaximumHeight() {
+		return $this->tsValue('maximumHeight');
+	}
+
+	/**
+	 * AllowCropping
+	 *
+	 * @return boolean
+	 */
+	public function getAllowCropping() {
+		return $this->tsValue('allowCropping');
+	}
+
+	/**
+	 * AllowUpScaling
+	 *
+	 * @return boolean
+	 */
+	public function getAllowUpScaling() {
+		return $this->tsValue('allowUpScaling');
+	}
+
+	/**
+	 * Returns the processed image path and dimensions
+	 *
+	 * @return array Image src and dimensions (keys "src", "width", "height")
+	 */
+	public function evaluate() {
+		$asset = $this->getAsset();
+
+		$maximumWidth = $this->getMaximumWidth();
+		$maximumHeight = $this->getMaximumHeight();
+		$allowCropping = $this->getAllowCropping();
+		$allowUpScaling = $this->getAllowUpScaling();
+
+		if (!$asset instanceof AssetInterface) {
+			throw new \Exception('No asset given for rendering.', 1435754424);
+		}
+		return $this->assetService->getThumbnailUriAndSizeForAsset($asset, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling);
+	}
+}
