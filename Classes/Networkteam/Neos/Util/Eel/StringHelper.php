3025850c211c9608a765ddde759eaf690c041fec
Sebastian Flor <sebastian.flor@networkteam.com>
2018-03-11 15:26:16 +0100

    Fix: whitespace

--- Classes/Networkteam/Neos/Util/Eel/StringHelper.php
+++ Classes/Networkteam/Neos/Util/Eel/StringHelper.php
@@ -10,13 +10,13 @@ use Neos\Neos\Exception;
  */
 class StringHelper implements ProtectedContextAwareInterface
 {
-	/**
-	 * @param String $string
+    /**
+     * @param String $string
      * @param Boolean $is_xhtml
-	 * @return String
+     * @return String
      * @throws Exception
-	 */
-	public function nl2br(string $string, bool $is_xhtml = true) {
+     */
+    public function nl2br(string $string, bool $is_xhtml = true) {
 
         if (!is_string($string)) {
             throw new Exception(sprintf('String expected by this helper, given: "%s".', gettype($string)), 1520777668);
@@ -24,11 +24,11 @@ class StringHelper implements ProtectedContextAwareInterface
 
         $newString = nl2br($string, $is_xhtml);
 
-		return $newString;
-	}
+        return $newString;
+    }
 
-	public function allowsCallOfMethod($methodName) {
-		return true;
-	}
+    public function allowsCallOfMethod($methodName) {
+        return true;
+    }
 
 }
