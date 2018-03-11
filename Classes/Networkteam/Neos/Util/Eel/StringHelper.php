173603cfa4402126c261665ba216a84d1bd9cbee
Sebastian Flor <sebastian.flor@networkteam.com>
2018-03-11 15:23:57 +0100

    Task:Add nl2br eel helper

---
+++ Classes/Networkteam/Neos/Util/Eel/StringHelper.php
@@ -0,0 +1,34 @@
+<?php
+namespace Networkteam\Neos\Util\Eel;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Eel\ProtectedContextAwareInterface;
+use Neos\Neos\Exception;
+
+/**
+ * Custom String helper
+ */
+class StringHelper implements ProtectedContextAwareInterface
+{
+	/**
+	 * @param String $string
+     * @param Boolean $is_xhtml
+	 * @return String
+     * @throws Exception
+	 */
+	public function nl2br(string $string, bool $is_xhtml = true) {
+
+        if (!is_string($string)) {
+            throw new Exception(sprintf('String expected by this helper, given: "%s".', gettype($string)), 1520777668);
+        }
+
+        $newString = nl2br($string, $is_xhtml);
+
+		return $newString;
+	}
+
+	public function allowsCallOfMethod($methodName) {
+		return true;
+	}
+
+}
