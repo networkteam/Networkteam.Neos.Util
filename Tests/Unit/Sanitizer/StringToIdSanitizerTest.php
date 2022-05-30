9a8052c6cfa5cbb2790def5ddfe0568d3aec0153
Nils Jung <nils.jung@networkteam.com>
2022-05-30 16:13:00 +0200

    adapt phpunit tests to work properly

--- Tests/Unit/Sanitizer/StringToIdSanitizerTest.php
+++ Tests/Unit/Sanitizer/StringToIdSanitizerTest.php
@@ -4,7 +4,10 @@ namespace Networkteam\Neos\Util\Tests\Unit\Sanitizer;
 /***************************************************************
  *  (c) 2014 networkteam GmbH - all rights reserved
  ***************************************************************/
-class StringToIdSanitizerTest extends \PHPUnit_Framework_TestCase {
+
+use Neos\Flow\Tests\UnitTestCase;
+
+class StringToIdSanitizerTest extends UnitTestCase {
 
 	protected $validIdRegex = '([A-Za-z])(A-Za-z0-9)*';
 
@@ -15,7 +18,7 @@ class StringToIdSanitizerTest extends \PHPUnit_Framework_TestCase {
 	public function stringIsSanitizedAgainstRegex($string) {
 		$sanitizer = new \Networkteam\Neos\Util\Sanitizer\StringToIdSanitizer();
 		$id = $sanitizer->convertToId($string);
-		$this->assertRegExp('/' . $this->validIdRegex . '/', $id);
+		$this->assertMatchesRegularExpression('/' . $this->validIdRegex . '/', $id);
 	}
 
 	/**
